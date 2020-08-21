a8515d8d37ac29faab2933bd8922da2264af37a8
Jun-cloud <kjy8827@gmail.com>
2020-08-21 20:32:10 +0900

    toolkit - createReducer

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import {createStore} from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
@@ -8,6 +8,7 @@ console.log(addToDo, deleteToDo);
 console.log(addToDo(), deleteToDo());
 console.log(addToDo.type, deleteToDo.type);
 
+/*
 const reducer = (state = [], action) => {
     switch(action.type) {
         case addToDo.type:
@@ -18,6 +19,24 @@ const reducer = (state = [], action) => {
             return state;
     }
 }
+*/
+
+// createReducer는 기본적으로 state를 변경하거나, 새로운 state를 return하는 역할을 함
+const reducer = createReducer([], {
+    // addToDo : state를 변경
+    [addToDo] : (state, action) => {
+        /* 
+            createReducer는 기본 redux와는 다르게 state를 변경할 수 있음
+            그 이유는 redux-toolkit이 자체적으로 { text: action.payload, id: Date.now() }, ...state 
+            이러한 작업을 해주기 때문
+        */
+        state.push({ text: action.payload, id: Date.now() });
+    },
+    // deleteToDo : 새로운 state를 return
+    [deleteToDo] : (state, action) => 
+        state.filter(toDo => toDo.id !== action.payload)
+})
+
 
 const store = createStore(reducer);
 
