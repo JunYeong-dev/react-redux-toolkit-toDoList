9e23ec3620ad013e9e1414f227068ce22b5b4a76
Jun-cloud <kjy8827@gmail.com>
2020-08-22 21:42:39 +0900

    redux developer tools

--- src/store.js
+++ src/store.js
@@ -1,5 +1,4 @@
-import {createStore} from "redux";
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createAction, createReducer, configureStore } from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
@@ -7,19 +6,6 @@ const deleteToDo = createAction("DELETE");
 console.log(addToDo, deleteToDo);
 console.log(addToDo(), deleteToDo());
 console.log(addToDo.type, deleteToDo.type);
-
-/*
-const reducer = (state = [], action) => {
-    switch(action.type) {
-        case addToDo.type:
-            return [{ text: action.payload, id: Date.now() }, ...state];
-        case deleteToDo.type:
-            return state.filter(toDo => toDo.id !== action.payload);
-        default:
-            return state;
-    }
-}
-*/
 
 // createReducer는 기본적으로 state를 변경하거나, 새로운 state를 return하는 역할을 함
 const reducer = createReducer([], {
@@ -37,8 +23,8 @@ const reducer = createReducer([], {
         state.filter(toDo => toDo.id !== action.payload)
 })
 
-
-const store = createStore(reducer);
+// configureStore : redux developer tools을 사용할 수 있게됨(꼭 toolkit이 필요하지는 않음; 크롬 확장프로그램 Redux DevTools 필요)
+const store = configureStore({ reducer });
 
 export const actionCreators = {
     addToDo,
